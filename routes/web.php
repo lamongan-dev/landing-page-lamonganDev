e604dbfbfb845d72bd43b56f2bfa9db1346633d8
LukmanHadi20 <lukmanhadi2000@gmail.com>
2026-01-22 15:19:24 +0700

    wip about us

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('welcome');
+});
+
 Route::match(['GET','POST'], '/_boost/browser-logs', function (Request $request) {
     return response()->json(['ok' => true]);
-});
+})->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
